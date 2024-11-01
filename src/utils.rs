6afeddd4be4f0a82f1f4ae4db529fd157fef529b
Sergio Ribera <56278796+SergioRibera@users.noreply.github.com>
2024-11-01 16:48:39 -0400

    chore: util to load font by glyph

--- src/utils.rs
+++ src/utils.rs
@@ -1,7 +1,22 @@
 mod hex_rgb;
 
+use font_kit::font::Font;
+use font_kit::source::SystemSource;
 pub use hex_rgb::ToColor;
 
 pub fn ease_out_cubic(x: f32) -> f32 {
     1.0 - (1.0 - x).powi(3)
+}
+
+pub fn load_font(content: char) -> Font {
+    let source = SystemSource::new();
+    source
+        .all_fonts()
+        .unwrap()
+        .into_iter()
+        .filter_map(|f| f.load().ok()?.glyph_for_char(content).map(|_| f))
+        .next()
+        .unwrap_or_else(|| panic!("Cannot found Font Family for glyph: {content}"))
+        .load()
+        .expect("Cannot load font")
 }
