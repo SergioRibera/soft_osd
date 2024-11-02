963a0109a78ffde51146f19a6976924dc83c8937
Sergio Ribera <56278796+SergioRibera@users.noreply.github.com>
2024-11-01 23:59:36 -0400

    feat: add lighten color function

--- src/utils.rs
+++ src/utils.rs
@@ -8,6 +8,16 @@ pub fn ease_out_cubic(x: f32) -> f32 {
     1.0 - (1.0 - x).powi(3)
 }
 
+pub fn lighten_color(r: u8, g: u8, b: u8, factor: f32) -> (u8, u8, u8) {
+    let factor = factor.clamp(0.0, 1.0);
+
+    let lighten = |component: u8| -> u8 {
+        ((component as f32) + ((255.0 - component as f32) * factor)).round() as u8
+    };
+
+    (lighten(r), lighten(g), lighten(b))
+}
+
 pub fn load_font(content: char) -> Font {
     let source = SystemSource::new();
     source
